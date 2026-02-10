f5d47ce4e12cb7ef0283c85968fcd22578511c3b
Aditi20-Jain04 <Aditi.25058@stu.upes.ac.in>
2026-02-10 11:07:45 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter sorted elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[j]) {
+            j++;
+            a[j] = a[i];
+        }
+    }
+
+    printf("Array after removing duplicates: ");
+    for (int i = 0; i <= j; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
